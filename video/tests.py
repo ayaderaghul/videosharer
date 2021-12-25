3ba569765f23d27fd3d2b7a994cfcfd1b20d05f0
chi <nguyenlinhchi09@gmail.com>
2021-12-26 03:20:09 +0700

    test

--- video/tests.py
+++ video/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+from .models import Video
+
+class VideoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        u = User.objects.create(first_name='Lana', last_name='Jeans')
+        v = Video.objects.create(title='Hello world', 
+            content='Hello world content', video_url='youtube.com/abc', 
+            author=u)
+        self.assertEqual(v.title, 'Hello world')
+        self.assertEqual(v.author, u)
+
+
